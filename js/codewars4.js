e67d4e8de69dc405d333b388200e92b3aa9c1dfe
Virina Rugoo <virina.rugoo@gmail.com>
2022-06-08 22:27:58 +0400

    fourth codewars exercise added

---
+++ js/codewars4.js
@@ -0,0 +1,22 @@
+// Title: How good are you really?
+//
+// Question:
+// There was a test in your class and you passed it. Congratulations!
+// But you’re an ambitious person. You want to know if you’re better than the average student in your class.
+//
+// You receive an array with your peers’ test scores. Now calculate the average and compare your score!
+//
+// Return True if you’re better, else False!
+//
+// Note:
+// Your points are not included in the array of your class’s points. For calculating the average point you may add your point to the given array!
+
+Solution:
+
+function betterThanAverage(classPoints, yourPoints){
+
+  const average = classPoints.reduce((x, y) => x + y, 0) / classPoints.length;
+
+  return average > yourPoints ? false : true
+
+}
