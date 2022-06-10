f4828cb0f652d88054af62c957d529b24c17412a
Virina Rugoo <virina.rugoo@gmail.com>
2022-06-10 20:07:25 +0400

    codewars5 in the basket repo

---
+++ js/codewars5.js
@@ -0,0 +1,14 @@
+// Title: Beginner-Reduce but Grow
+//
+// // QUESTION:
+//
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+//
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+Solution
+
+function grow(x){
+  return x.reduce((res,n)=> res*n);
+}
