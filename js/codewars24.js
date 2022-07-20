cc1863708f8a32f8104af890d2cc21417e0a192c
Virina Rugoo <virina.rugoo@gmail.com>
2022-07-20 20:14:12 +0400

    convert a string to an array codewars

---
+++ js/codewars24.js
@@ -0,0 +1,18 @@
+// TITLE: Convert a string to an array
+//
+// DESCRIPTION:
+// Write a function to split a string and convert it into an array of words.
+//
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+//
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+// .
+
+Solution:
+
+function stringToArray(string){
+
+	// code code code
+  return string.trim().split(" ");
+}
