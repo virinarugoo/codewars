b38130d7e67e8d364e2423f6c1c34390cb34f3e8
Virina Rugoo <virina.rugoo@gmail.com>
2022-07-22 21:17:30 +0400

    lost without a map codewars exercises

---
+++ js/codewars25.js
@@ -0,0 +1,14 @@
+// TITLE: Beginner - Lost Without a Map
+//
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+//
+// For example:
+//
+// [1, 2, 3] --> [2, 4, 6]
+
+Solution:
+
+function maps(x){
+  return x.map(v => v*2);
+}
