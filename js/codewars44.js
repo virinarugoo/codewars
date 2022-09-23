327f4eea76cfbbb3363399b1da0bb12d3f313c46
Virina Rugoo <virina.rugoo@gmail.com>
2022-09-23 20:52:46 +0400

    you can't code under pressure

---
+++ js/codewars44.js
@@ -0,0 +1,9 @@
+// TITLE:You Can't Code Under Pressure #1
+
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i*2;
+}
