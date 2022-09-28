016dcc7a24d6a7964515aa345fa17f63439a1fcd
Virina Rugoo <virina.rugoo@gmail.com>
2022-09-28 18:54:53 +0400

    string templates-bug fixing

---
+++ js/codewars48.js
@@ -0,0 +1,10 @@
+// TITLE:String Templates - Bug Fixing #5
+
+// DESCRIPTION:
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+//
+// function cockroachSpeed(s) {
+  //Good Luck!
+  function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
