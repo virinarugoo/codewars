ca884562746fbab1a94c4b90f1c10ba6a35c3218
Virina Rugoo <virina.rugoo@gmail.com>
2022-10-01 12:24:12 +0400

    methods of string object- last letter lowercase

---
+++ js/codewars51.js
@@ -0,0 +1,21 @@
+// TITLE:Training JS #19: Methods of String object--toUpperCase() toLowerCase() and replace()
+
+// DESCRIPTION:
+// Coding in function alienLanguage, function accept 1 parameter:str. str is a sentence.
+//
+// We translate the sentence into an alien language according to the following rules:
+//
+// Each word in the sentence is separated by spaces. The last letter of each word in the sentence turns to lowercase, and the other letters are capitalized. Looks very strange? Because this is the form of alien language ;-)
+//
+// for example:
+//
+// alienLanguage("My name is John") should return "My NAMe Is JOHn"
+// alienLanguage("this is an example") should return "THIs Is An EXAMPLe"
+// alienLanguage("Hello World") should return "HELLo WORLd"
+// A small hint: The first conversion of the entire string will make the code easier
+
+
+function alienLanguage(str){
+  //coding here...
+  return str.split(' ').map(v=>v.slice(0,-1).toUpperCase() +v.slice(-1).toLowerCase()).join(' ');
+}
