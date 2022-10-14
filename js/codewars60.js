f98acdbe0ff76a61f918a6ea897a1a428f0121cc
Virina Rugoo <virina.rugoo@gmail.com>
2022-10-14 23:30:30 +0400

    convert boolean to string

---
+++ js/codewars60.js
@@ -0,0 +1,12 @@
+// TITLE: Convert a Boolean to a String
+
+//
+// // DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+//
+// Note: Only valid inputs will be given.
+//
+
+function booleanToString(b){
+  return b.toString();
+}
