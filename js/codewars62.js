52555017e7b7c167480c65a60ce7f2e4fef04a09
Virina Rugoo <virina.rugoo@gmail.com>
2022-10-16 18:23:00 +0400

    grasshopper personalized msg

---
+++ js/codewars62.js
@@ -0,0 +1,21 @@
+// TITLE: Grasshopper - Personalized Message
+
+
+// DESCRIPTION:
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+//
+// Use conditionals to return the proper message:
+//
+// case	return
+// name equals owner	'Hello boss'
+// otherwise
+
+function greet (name, owner) {
+  // Add code here
+  if(name === owner) {
+     return "Hello boss"
+     }
+  else {
+    return "Hello guest"
+  }
+}
