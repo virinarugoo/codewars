71517e677a3910b7d5e0406293e4ca6587bf1a8d
Virina Rugoo <virina.rugoo@gmail.com>
2023-01-18 17:27:05 +0400

    reversed sequence

---
+++ js/codewars98.js
@@ -0,0 +1,13 @@
+// TITLE:Reversed sequence
+
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+//
+// Example : n=5 --> [5,4,3,2,1]
+
+
+function remove (string) {
+  //coding and coding....
+  let greet = string.replace(/!$/, '')
+  return greet;
+}
